5c8345974c16f8f2b6790c4b47c2e784e21de11e
Johann <mast3r1sgr3at@hotmail.com>
2020-09-03 19:46:50 +0800

    Location module COMPLETED

--- lib/Util/Quick.dart
+++ lib/Util/Quick.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tripwire/Model/CurrentLocation.dart';
+import 'package:weather/weather.dart';
 
 class Quick {
   static Widget makeText(String text, int color, double size,
@@ -35,6 +37,44 @@ class Quick {
           currentPos.latitude, currentPos.longitude);
 
       return placeList[0];
+    });
+  }
+
+  static Future<Weather> getWeather(Placemark place) {
+    Weather weather;
+    Geolocator geolocator = new Geolocator();
+    return geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position currentPos) {
+      return new WeatherFactory(CurrentData.weatherAPIkey)
+          .currentWeatherByLocation(currentPos.latitude, currentPos.longitude)
+          .then((Weather currentWeather) {
+        weather = currentWeather;
+
+        if (weather == null) {
+          //cannot obtain by coords, try by city
+          return new WeatherFactory(CurrentData.weatherAPIkey)
+              .currentWeatherByCityName(place.locality)
+              .then((Weather currentWeather) {
+            weather = currentWeather;
+            return weather;
+          });
+        }
+        if (weather == null) {
+          //cannot obtain by city, try by state
+          return new WeatherFactory(CurrentData.weatherAPIkey)
+              .currentWeatherByCityName(place.administrativeArea)
+              .then((Weather currentWeather) {
+            weather = currentWeather;
+            return weather;
+          });
+        }
+
+        return weather;
+
+      });
+
+
     });
   }
 }
